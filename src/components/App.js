cd57290ae692d74ec24d4ed285b13a417e0fea19
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-02-23 20:49:07 -0700

    commit tree 04496dbd677e09bbdcafc2b754ce8f1e38a0b16b

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
-import ShoppingList from "./ShoppingList";
+import ShoppingList from "/Users/robertedmonds/Development/code/Mod2/State/react-hooks-information-flow-lab/src/components/ShoppingList.js";
 import itemData from "../data/items";
+import Header from "/Users/robertedmonds/Development/code/Mod2/State/react-hooks-information-flow-lab/src/components/Header.js";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+  // function handleDarkModeClick() {
+  //   setIsDarkMode((isDarkMode) => !isDarkMode);
+  // }
+  function onDarkModeClick(){
+    setIsDarkMode((isDarkMode) => !isDarkMode)
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+      <Header onDarkModeClick={onDarkModeClick} isDarkMode={isDarkMode}/>
       <ShoppingList items={itemData} />
     </div>
   );
